a3f26aff4e941926757e30e2d146fac761e3bcc3
ZB Siu <siuzhuobin@gmail.com>
2025-09-11 11:50:00 +0800

    copied and pasted x106main into x103main for the streamlit cloud hoisting

--- x103.py
+++ x103.py
@@ -1,3 +1,4 @@
+# actually is x106 just that the streamlit thing doesn't allow to change the filename
 import streamlit as st
 
 pg = st.navigation([st.Page("x105SingleVariables.py",title='Numerical variables'),
